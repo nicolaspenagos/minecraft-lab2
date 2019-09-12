829b928398c2826351a1cdb5f45ffb362ef058bd
Nicolas Penagos <nikopm98@hotmail.com>
2019-09-12 16:05:47 -0500

    Images Views and buttons have been set

--- src/userinterface/GUIController.java
+++ src/userinterface/GUIController.java
@@ -2,8 +2,114 @@ package userinterface;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 public class GUIController {
+
+    @FXML
+    private ImageView imv11;
+
+    @FXML
+    private ImageView imv12;
+
+    @FXML
+    private ImageView imv13;
+
+    @FXML
+    private ImageView imv14;
+
+    @FXML
+    private ImageView imv15;
+
+    @FXML
+    private ImageView imv16;
+
+    @FXML
+    private ImageView imv17;
+
+    @FXML
+    private ImageView imv18;
+
+    @FXML
+    private ImageView imv19;
+
+    @FXML
+    private ImageView imv29;
+
+    @FXML
+    private ImageView imv28;
+
+    @FXML
+    private ImageView imv27;
+
+    @FXML
+    private ImageView imv26;
+
+    @FXML
+    private ImageView imv25;
+
+    @FXML
+    private ImageView imv24;
+
+    @FXML
+    private ImageView imv22;
+
+    @FXML
+    private ImageView imv21;
+
+    @FXML
+    private ImageView imv31;
+
+    @FXML
+    private ImageView imv32;
+
+    @FXML
+    private ImageView imv33;
+
+    @FXML
+    private ImageView imv34;
+
+    @FXML
+    private ImageView imv35;
+
+    @FXML
+    private ImageView imv36;
+
+    @FXML
+    private ImageView imv37;
+
+    @FXML
+    private ImageView imv38;
+
+    @FXML
+    private ImageView imv39;
+
+    @FXML
+    private ImageView imv49;
+
+    @FXML
+    private ImageView imv48;
+
+    @FXML
+    private ImageView imv47;
+
+    @FXML
+    private ImageView imv46;
+
+    @FXML
+    private ImageView imv45;
+
+    @FXML
+    private ImageView imv44;
+
+    @FXML
+    private ImageView imv43;
+
+    @FXML
+    private ImageView imv42;
+
+    @FXML
+    private ImageView imv41;
 
     @FXML
     void add(ActionEvent event) {
