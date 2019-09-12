93c29802182833e0a9cdb76ad7747a845e1b9513
Nicolas Penagos <nikopm98@hotmail.com>
2019-09-12 01:12:33 -0500

    The user interface has been created

---
+++ src/userinterface/GUIController.java
@@ -0,0 +1,33 @@
+package userinterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GUIController {
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateBlock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void next(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reestart(ActionEvent event) {
+
+    }
+
+}
--- src/userinterface/Main.java
+++ src/userinterface/Main.java
@@ -1,5 +1,23 @@
 package userinterface;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml")); 
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("MINECRAFT");
+		stage.show();
+	}
 
 }
