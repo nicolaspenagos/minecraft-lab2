77d095649c97a720393d5a648ae7f60e31719d73
Mateo Loaiza <thetraceurs@gmail.com>
2019-09-14 16:32:58 -0500

    Back in time

--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -1,19 +1,5 @@
 package model;
 
-import stacks.StackA;
-import stacks.StackL;
+public class Inventory {
 
-public class Inventory<Cube> {
-
-	private StackL<Cube> s;
-	private StackA a;
-	
-	public Inventory() {
-		
-	}
-	
-	public void nextStack() {
-		
-	}
-	
 }
